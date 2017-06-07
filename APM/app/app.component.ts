50b817fdfdf25ad14825450d9f61466f4c2f8582
Fulvio Almeida <fulvioalmeidasilva@gmail.com>
2017-06-06 22:27:05 -0300

    added new template product-list and modules

--- APM/app/app.component.ts
+++ APM/app/app.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'pm-app',
     template: `
-        <h1>Angular2: Getting Started</h1>
+        <h1>{{pageTitle}}</h1>
+       <p> My first application in angular 2</p>
     `
 })
-export class AppComponent { }
+export class AppComponent {
+    pageTitle:string ='Acme Product Management';
+}
